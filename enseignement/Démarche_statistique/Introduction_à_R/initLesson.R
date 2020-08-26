9f126a82d2d5a27571002d1de0079bc21494c924
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-08-26 12:08:24 +0200

    clean enseignement repertoire

--- enseignement/Démarche_statistique/Introduction_à_R/initLesson.R
+++
@@ -1,9 +0,0 @@
-.get_course_path <- function(){
-  tryCatch(swirl:::swirl_courses_dir(),
-           error = function(c) {file.path(find.package("swirl"),"Courses")}
-  )
-}
-
-dta <- read.table(
-  file.path(.get_course_path(), "Démarche_statistique", "Introduction_à_R", "usair.txt"), 
-                  sep = ',', header = TRUE, skip = 8)
