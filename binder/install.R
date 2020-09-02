e7d20a776dfd94e9afe1a8e4f4cd170408b8c413
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-09-02 21:59:03 +0200

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,7 +1,13 @@
-install.packages("JuliaCall")
-library(JuliaCall)
-julia <- julia_setup()
-install.packages("diffeqr")
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org"
+  options(repos = r)
+})
+
 install.packages("sbm")
 install.packages("palmerpenguins")
 
+devtools::install_github("Non-Contradiction/JuliaCall")
+library(JuliaCall)
+julia <- julia_setup()
+install.packages("diffeqr")
