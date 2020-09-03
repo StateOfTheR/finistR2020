468dfead1fb4677d88ca58870642a40e5ae52fd7
jchiquet <julien.chiquet@gmail.com>
2020-09-03 09:58:14 +0200

    first attempt for including optim contribution

--- binder/install.R
+++ binder/install.R
@@ -7,6 +7,10 @@ local({
 install.packages("sbm")
 install.packages("palmerpenguins")
 
+## optim 
+devtools::install_github("StateOfTheR/optimLibR")
+remotes::install_github("mlverse/torch")
+
 devtools::install_github("Non-Contradiction/JuliaCall")
 library(JuliaCall)
 julia <- julia_setup()
