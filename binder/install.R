dd01d53304259169a66d2b0fa9a7134b7587161f
jchiquet <julien.chiquet@gmail.com>
2020-09-03 09:59:43 +0200

    added rkheops to binder config

--- binder/install.R
+++ binder/install.R
@@ -10,6 +10,7 @@ install.packages("palmerpenguins")
 ## optim 
 devtools::install_github("StateOfTheR/optimLibR")
 remotes::install_github("mlverse/torch")
+install.packages("rkeops")
 
 devtools::install_github("Non-Contradiction/JuliaCall")
 library(JuliaCall)
