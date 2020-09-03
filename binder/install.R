6974cc0cb64b8817c5cacc8f4b1208654d8e877b
Julien Chiquet <julien.chiquet@gmail.com>
2020-09-03 14:57:41 +0200

    adding dependencies install.R, removing from environment.yml + swirl.Rmd

--- binder/install.R
+++ binder/install.R
@@ -4,10 +4,16 @@ local({
   options(repos = r)
 })
 
-## non-CRAN packages
+## non CRAN packages
 devtools::install_github("https://github.com/rstudio-education/gradethis")
 devtools::install_github("StateOfTheR/optimLibR")
 devtools::install_github("mlverse/torch")
+
+## CRAN packages not found in conda
+install.packages("rkeops")
+install.packages("sbm")
+install.packages("swirlify")
+install.packages("palmerpenguins")
 
 ## Julia and co
 devtools::install_github("Non-Contradiction/JuliaCall")
