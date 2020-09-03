9a511800608e88f8ce9193a4301e475592701234
jchiquet <jchiquet@users.noreply.github.com>
2020-09-03 15:04:47 +0000

    deploy: 84a22b7f3db70ca880b682620ce8957b0397125b

--- build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/binder/install.R
+++ build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/build/binder/install.R
@@ -4,9 +4,18 @@ local({
   options(repos = r)
 })
 
+## non CRAN packages
+devtools::install_github("https://github.com/rstudio-education/gradethis")
+devtools::install_github("StateOfTheR/optimLibR")
+devtools::install_github("mlverse/torch")
+
+## CRAN packages not found in conda
+install.packages("rkeops")
 install.packages("sbm")
+install.packages("swirlify")
 install.packages("palmerpenguins")
 
+## Julia and co
 devtools::install_github("Non-Contradiction/JuliaCall")
 library(JuliaCall)
 julia <- julia_setup()
